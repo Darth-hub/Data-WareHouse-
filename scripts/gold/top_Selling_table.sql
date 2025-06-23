21d646ec36b0a1227819e8849600b34e28af8c43
Ayush Ranjan <ayushranjan112400@gmail.com>
2025-06-23 20:33:58 +0530

    Create top_Selling_table.sql

---
+++ scripts/gold/top_Selling_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS gold_top_selling_products (
+        product_id VARCHAR(50),
+        product_name VARCHAR(255),
+        total_sales_amount INT,
+        total_quantity_sold INT
+    );
